5d3bb5123d37a7457e7bfaa595a1f01ff4ea0532
sachin shukla <sachin55shu@gmail.com>
2025-07-05 01:16:57 +0530

    Updated git_commands.py with more commands

--- git_commands.py
+++ git_commands.py
@@ -6,7 +6,33 @@ rm testing.py
 git restore testing.py
 git rm --cached testing.py
 git add.
-sachin shukla 
-yesh chande 
-drop shot
-box game
+git restore git_commands.py
+git remote add origin https://github.com/sachinshukla111/github-for-devops-workshop
+git push origin main
+git push origin main
+git remote add origin https://github.com/sachinshukla111/github-for-devops-workshop
+git push origin main
+git restore command.txt
+git add git_commands.py
+git status
+git commit -m "added commands file"
+git push origin main
+fit restore git_commands.txt
+git restore git_commands.txt
+git push origin main
+rm git_commands.txt
+git  rm git_commands.txt
+git push origin main
+git commit -m "added new commands "
+git push origin main
+git remote -v
+git push origin master
+git remote add origin https://github.com/sachinshukla111/github-for-devops-workshop
+git branch
+git branch -m main master
+git commit -m "Updated git_commands.py with more commands"git push origin --delete main
+git add git_commands.py
+git commit -m "Added more Git commands"
+git push origin master
+
+
