4ff119fbe5a8870802a2a0a7e54c7de1e8fa06f9
sachinshukla111 <sachin55shu@gmail.com>
2025-07-05 08:34:38 +0530

    Update git_commands.py

--- git_commands.py
+++ git_commands.py
@@ -9,22 +9,14 @@ git add.
 git restore git_commands.py
 git remote add origin https://github.com/sachinshukla111/github-for-devops-workshop
 git push origin main
-git push origin main
-git remote add origin https://github.com/sachinshukla111/github-for-devops-workshop
-git push origin main
 git restore command.txt
 git add git_commands.py
 git status
 git commit -m "added commands file"
-git push origin main
 fit restore git_commands.txt
-git restore git_commands.txt
 git push origin main
 rm git_commands.txt
 git  rm git_commands.txt
-git push origin main
-git commit -m "added new commands "
-git push origin main
 git remote -v
 git push origin master
 git remote add origin https://github.com/sachinshukla111/github-for-devops-workshop
@@ -33,6 +25,8 @@ git branch -m main master
 git commit -m "Updated git_commands.py with more commands"git push origin --delete main
 git add git_commands.py
 git commit -m "Added more Git commands"
-git push origin master
+
+i made some new changes
+
 
 
